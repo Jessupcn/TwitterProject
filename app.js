b92cf2b510662eab48ff6f0d99924670153a01ae
Connor Jessup <cjessup@umich.edu>
2018-03-05 18:03:09 -0500

    end of day commital

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
-app.get('/', (req, res) => res.send(`Hi there, Big Brother is watching you`));
+app.use((req, res, next) => {
+    console.log(app.res, app.req);
+    next();
+});
 
+app.get('/', (req, res) => res.send(`Hi there, Big Brother is watching you`));
 
 app.listen(3000, () => console.log('Server is listening....... intently...'));
