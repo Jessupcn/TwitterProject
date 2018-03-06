ffe19c2196730c28a996a74a04ba96367fef8097
Connor Jessup <cjessup@umich.edu>
2018-03-06 10:38:37 -0500

    completed more steps 46%

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 
 app.use(volleyball);
 
-app.get('/', (req, res) => res.send(`Hi there, Big Brother is watching you`));
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Frodo' },
+        { name: 'Gandalf' },
+        { name: 'Legolas' }
+    ],
+}
+
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, (err, output) => {
+    if(err){console.log(err)}
+    console.log(output);
+});
+
+// res.render('index', {title: 'LOTR Badasses', people: locals.people})
+
+app.get('/', (req, res) => res.render('index', {title: 'LOTR Badasses', people: locals.people}));
 
 app.listen(3000, () => console.log('Server is listening....... intently...'));
